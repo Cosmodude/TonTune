233acd0afee86e2e359d075bf1c6ad3b15c20e5e
Cosmodude <vladislav.lenskii@gmail.com>
2023-10-06 15:37:23 +0900

    deployment script ready

--- scripts/deployTuneNft.ts
+++ scripts/deployTuneNft.ts
@@ -1,13 +1,31 @@
-import { toNano } from 'ton-core';
+import { Address, beginCell, toNano } from 'ton-core';
+import { buildNftCollectionContentCell } from './contentUtils/off-chain';
 import { TuneNft } from '../wrappers/TuneNft';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const tuneNft = provider.open(TuneNft.createFromConfig({}, await compile('TuneNft')));
 
-    await tuneNft.sendDeploy(provider.sender(), toNano('0.05'));
+    const nftCollection = provider.open(TuneNft.createFromConfig({
+        ownerAddress: provider.sender().address as Address,
+        nextItemIndex: 0,
+        collectionContent: buildNftCollectionContentCell(
+            {
+                collectionContent: '',
+                commonContent: ''
+            }
+        ),
+        nftItemCode: await compile('NftItem'),
+        royaltyParams: {
+            royaltyFactor: 15,
+            royaltyBase: 100,
+            royaltyAddress: provider.sender().address as Address
+        }
+        
+    }, await compile('NftCollection')));
 
-    await provider.waitForDeploy(tuneNft.address);
+    await nftCollection.sendDeploy(provider.sender(), toNano('0.05'));
 
-    // run methods on `tuneNft`
+    await provider.waitForDeploy(nftCollection.address);
+
+    // run methods on `nftCollection`
 }
