af182b1bb350d0d38d53f0d3d9cfd714efb30da8
Cosmodude <vladislav.lenskii@gmail.com>
2023-12-21 11:01:02 +0900

    tests created

--- tests/TuneNft.spec.ts
+++ tests/TuneNft.spec.ts
@@ -1,8 +1,9 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { Cell, toNano } from 'ton-core';
+import { Cell, toNano, Address } from 'ton-core';
 import { TuneNft } from '../wrappers/TuneNft';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
+import { buildCollectionContentCell } from '../scripts/contentUtils/off-chain';
 
 describe('TuneNft', () => {
     let code: Cell;
@@ -13,13 +14,30 @@ describe('TuneNft', () => {
 
     let blockchain: Blockchain;
     let tuneNft: SandboxContract<TuneNft>;
+    let deployer;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        tuneNft = blockchain.openContract(TuneNft.createFromConfig({}, code));
+        deployer = await blockchain.treasury('deployer');
 
-        const deployer = await blockchain.treasury('deployer');
+        tuneNft = blockchain.openContract(TuneNft.createFromConfig({
+            ownerAddress: deployer.address as Address,
+            nextItemIndex: 0,
+            collectionContent: buildCollectionContentCell(
+                {
+                    collectionContent: 'https://raw.githubusercontent.com/Cosmodude/TonTune/main/collectionMetadata.json',
+                    commonContent: ''
+                }
+            ),
+            nftItemCode: await compile('NftItem'),
+            royaltyParams: {
+                royaltyFactor: Math.floor(Math.random() * 100),
+                royaltyBase: 100,
+                royaltyAddress: deployer.address as Address
+            }
+            
+        }, code));
 
         const deployResult = await tuneNft.sendDeploy(deployer.getSender(), toNano('0.05'));
 
